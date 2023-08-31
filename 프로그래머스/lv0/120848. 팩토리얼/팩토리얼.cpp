021818a62574500b9ce171986a915717ebc0b3a2
LeeHyungJoo <prism9411@gmail.com>
2023-08-31 12:25:58 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+   int answer = 0;
+
+    int fac = 1;
+    while (true)
+    {
+        answer++;
+        fac *= answer;
+
+        if (fac == n)
+            break;
+
+        if (fac > n)
+        {
+            answer--;
+            break;
+        }
+    }
+    return answer;
+}
