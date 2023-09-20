293c76af367fd54750ce7d4a0f1e0d766a99b4a1
LeeHyungJoo <prism9411@gmail.com>
2023-09-20 10:50:13 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -1,40 +1,19 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
+#include <regex>
 
 using namespace std;
 
 int solution(string s) {
-	unordered_map<char, unordered_map<string, char>> check_map;
-	check_map['z']["zero"] = '0';
-	check_map['o']["one"] = '1';
-	check_map['t']["two"] = '2';
-	check_map['t']["three"] = '3';
-	check_map['f']["four"] = '4';
-	check_map['f']["five"] = '5';
-	check_map['s']["six"] = '6';
-	check_map['s']["seven"] = '7';
-	check_map['e']["eight"] = '8';
-	check_map['n']["nine"] = '9';
-
-	string result;
-	for (int idx = 0; idx < s.size(); idx++)
-	{
-		if (isdigit(s[idx])) result.push_back(s[idx]);
-
-		auto f_itr = check_map.find(s[idx]);
-		if (f_itr == check_map.end())
-			continue;
-
-		for (const auto& numstr : f_itr->second)
-		{
-			if (numstr.first != s.substr(idx, numstr.first.size()))
-				continue;
-
-			result.push_back(numstr.second);
-			break;
-		}
-	}
-
-	return atoi(result.c_str());
+	s = regex_replace(s, regex("zero"), "0");
+	s = regex_replace(s, regex("one"), "1");
+	s = regex_replace(s, regex("two"), "2");
+	s = regex_replace(s, regex("three"), "3");
+	s = regex_replace(s, regex("four"), "4");
+	s = regex_replace(s, regex("five"), "5");
+	s = regex_replace(s, regex("six"), "6");
+	s = regex_replace(s, regex("seven"), "7");
+	s = regex_replace(s, regex("eight"), "8");
+	s = regex_replace(s, regex("nine"), "9");
+	return atoi(s.c_str());
 }
