6176ef2b93be9b26e24ddb56b13abc3698f69ce4
LeeHyungJoo <prism9411@gmail.com>
2023-10-31 12:49:09 +0900

    [level unrated] Title: 카운트 다운, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.cpp
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.cpp
@@ -5,15 +5,7 @@ using namespace std;
 
 vector<int> solution(int start, int end_num) {
     vector<int> answer;
-    if(start < end_num)
-    {
-        for(int i = start ; i <= end_num ; i++)
-            answer.push_back(i);
-    }
-    else
-    {
-        for(int i = start ; i >= end_num ; i--)
-            answer.push_back(i);
-    }
+    for(int i = start ; i >= end_num ; i--)
+        answer.push_back(i);
     return answer;
 }
