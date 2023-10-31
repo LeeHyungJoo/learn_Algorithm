b909228b54c6da85b8e2ad3e7bbfd650f619c0fe
LeeHyungJoo <prism9411@gmail.com>
2023-10-31 15:32:42 +0900

    [level unrated] Title: 뒤에서 5등까지, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
@@ -6,7 +6,5 @@ using namespace std;
 
 vector<int> solution(vector<int> num_list) {
     sort(num_list.begin(), num_list.end());
-    vector<int> answer;
-    answer.assign(num_list.begin(), num_list.begin() +5);
-    return answer;
+    return {num_list.begin(), num_list.begin() +5};
 }
