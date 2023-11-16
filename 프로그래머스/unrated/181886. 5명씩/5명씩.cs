981b5a74d04df3ea722e298c1716fdff7dd858e5
LeeHyungJoo <prism9411@gmail.com>
2023-11-16 15:11:41 +0900

    [level unrated] Title: 5명씩, Time: 2.22 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.cs
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.cs
@@ -3,15 +3,8 @@ using System.Linq;
 using System.Collections.Generic;
 
 public class Solution {
-    public string[] solution(string[] names) {
-        List<string> answer = new List<string>();
-
-        for(int i = 0; i < names.Length; i++)
-        {
-            if(i % 5 == 0)
-                answer.Add(names[i]);
-        }
-
-        return answer.ToArray(); 
+    public string[] solution(string[] names)
+    {
+        return names.Where((name, index) => index % 5 == 0).ToArray();
     }
 }
