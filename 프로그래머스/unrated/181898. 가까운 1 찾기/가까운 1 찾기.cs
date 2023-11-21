da86fbce6f1508002523caf90ca07e207267c6f5
LeeHyungJoo <prism9411@gmail.com>
2023-11-21 12:10:45 +0900

    [level unrated] Title: 가까운 1 찾기, Time: 0.37 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.cs
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.cs
@@ -4,8 +4,6 @@ using System.Linq;
 
 public class Solution {
     public int solution(int[] arr, int idx) {
-        var nextIndex = arr.Skip(idx).Select((value, index) => new { Value = value, Index = index + idx })
-                                            .FirstOrDefault(x => x.Value == 1)?.Index ?? -1;
-        return nextIndex;
+        return Array.FindIndex(arr, idx, t => t == 1); 
     }
 }
