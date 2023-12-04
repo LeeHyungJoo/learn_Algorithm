c677d7aeea816a7f9ee4f1ca38dfca140dbd7127
LeeHyungJoo <prism9411@gmail.com>
2023-12-04 17:24:59 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <numeric>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <math.h>
+#include <bitset>
+#include <set>
+#include <regex>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+	
+	int x; 
+	cin >> x;
+	bitset<8> bits(x);
+	cout << bits.count();
+	return 0;
+}
