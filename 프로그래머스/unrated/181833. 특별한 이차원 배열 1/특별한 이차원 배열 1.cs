b267596f8b8e5e89710e6affb11678ea82281d73
LeeHyungJoo <prism9411@gmail.com>
2023-12-05 11:28:45 +0900

    [level unrated] Title: 특별한 이차원 배열 1, Time: 0.27 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cs
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cs
@@ -3,9 +3,8 @@ using System;
 public class Solution {
     public int[,] solution(int n) {
         int[,] answer = new int[n,n];
-        for(int i = 0; i < answer.GetLength(0); i++)
-            for(int j = 0; j < answer.GetLength(1); j++)
-                answer[i,j] = i == j ? 1 : 0;
+        for(int i = 0; i < n; i++)
+                answer[i,i] = 1;
 
         return answer;
     }
