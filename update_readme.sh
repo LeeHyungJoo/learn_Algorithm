f45987a6f5d82798d8427de98e114ab45b125ab5
LeeHyungJoo <prism9411@gmail.com>
2023-12-15 11:16:34 +0900

    Update update_readme.sh

--- update_readme.sh
+++ update_readme.sh
@@ -7,4 +7,4 @@ file_count=$(git ls-files '**/*.cpp' '**/*.cs' | wc -l)
 echo "Total number of .cpp and .cs files: $file_count"
 
 # Update file count after "Total Count of Solved" in README.md
-sed -E ":a;N;$!ba; s/(Total Count of Solved: )[0-9]+/\1$file_count/" README.md
+awk -v count="$file_count" '/Total Count of Solved/ { sub(/[0-9]+/, count); } 1' README.md > temp && mv temp README.md
