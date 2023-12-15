287061b03fcd9d467b78b7a12af6d7cf04d759d6
LeeHyungJoo <prism9411@gmail.com>
2023-12-15 10:46:31 +0900

    Update update_readme.sh

--- update_readme.sh
+++ update_readme.sh
@@ -1,5 +1,5 @@
 # Count the number of files
-file_count=$(($(git ls-files | wc -l) / 2))
+file_count=$(($(git ls-files | wc -l)) / 2)
 
 # Replace placeholder in README.md with the file count
 sed -i "s/{{FILE_COUNT}}/$file_count/g" README.md
