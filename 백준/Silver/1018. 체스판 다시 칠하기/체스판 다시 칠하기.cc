7a3f7b7c1b9c1ef2bad5d33bb8da2a6d6bb649d7
HyungJoo <prism9411@gmail.com>
2024-01-11 19:31:58 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -0,0 +1,94 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <numeric>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <math.h>
+#include <bitset>
+#include <set>
+#include <regex>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int M, N;
+char chess[50][50];
+
+
+int TakeMinCnt(int y, int x)
+{
+	int wCnt = 0, bCnt = 0;
+	for (int i = y; i < y + 8; i++)
+		for (int j = x; j < x + 8; j++)
+		{
+			char next = chess[i][j];
+
+			if (i & 1)
+			{
+				if (j & 1)
+				{
+					if (next == 'W')
+						bCnt++;
+					else
+						wCnt++;
+				}
+				else
+				{
+					if (next == 'B')
+						bCnt++;
+					else
+						wCnt++;
+				}
+			}
+			else
+			{
+				if (j & 1)
+				{
+					if (next == 'B')
+						bCnt++;
+					else
+						wCnt++;
+				}
+				else
+				{
+					if (next == 'W')
+						bCnt++;
+					else
+						wCnt++;
+				}
+			}
+		}
+
+	return min(wCnt, bCnt);
+}
+
+
+int main()
+{
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+		{
+			cin >> chess[i][j];
+		}
+
+	int minval = 9999999;
+	for (int i = 0; i < N - 8 + 1; i++)
+	{
+		for (int j = 0; j < M - 8 + 1; j++)
+		{
+			minval = min(TakeMinCnt(i, j), minval);
+		}
+	}
+	cout << minval;
+	return 0;
+}
