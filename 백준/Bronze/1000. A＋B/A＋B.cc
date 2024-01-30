915a5eaa6db656619c48c32e56009b6a770773ca
HyungJoo <prism9411@gmail.com>
2024-01-31 08:45:12 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <numeric>
+
+using namespace std;
+
+int A, B;
+
+int main()
+{
+	cin >> A >> B;
+	cout << A + B;
+	return 0;
+}
