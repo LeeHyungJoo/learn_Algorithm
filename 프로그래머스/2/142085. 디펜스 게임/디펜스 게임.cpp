73ccc6ea29aff3543c533814dc75d8963dfd84c9
HyungJoo <prism9411@gmail.com>
2024-12-09 14:51:58 +0900

    [level 2] Title: 디펜스 게임, Time: 25.23 ms, Memory: 40.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(int soldierCnt, int shieldCnt, vector<int> enemySoldierWaves) {
+	int round = 0;
+	priority_queue<int> PQ;
+
+	for (auto enemySoldierCnt : enemySoldierWaves)
+	{
+		PQ.push(enemySoldierCnt);
+		soldierCnt -= enemySoldierCnt;
+
+		if(soldierCnt < 0)
+		{
+			if (shieldCnt == 0)
+				break;
+
+			shieldCnt--;
+
+			if (!PQ.empty())
+			{
+				soldierCnt += PQ.top();
+				PQ.pop();
+			}
+		}
+
+		round++;
+	}
+
+	return round;
+}
